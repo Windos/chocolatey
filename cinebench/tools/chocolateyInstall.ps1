4b526b9dc3b308d224b857fa0dfeb1bf5f6b9468
Joshua King <windosbk@gmail.com>
2014-08-16 10:10:12 +1200

    Upadting and cleaning up comments in cinebench

---
+++ cinebench/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'cinebench'
+$url = 'http://http.maxon.net/pub/benchmarks/CINEBENCH_R15.zip'
+$md5Hash = '0a7353398a7f4b61f3b970c74feef726'
+
+try { 
+  $binRoot = "$env:systemdrive\tools"
+  if($env:chocolatey_bin_root -ne $null -and $env:chocolatey_bin_root -notlike '*:\*'){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot "$packageName"
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -checksum $md5Hash
+  Install-ChocolateyDesktopLink -TargetFilePath "$installDir\CINEBENCH Windows 64 Bit.exe"
+  
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
