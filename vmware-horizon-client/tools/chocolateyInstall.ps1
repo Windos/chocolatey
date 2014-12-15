1cd1e17340f7130971e610eb256a360cfdc06798
Joshua King <windosbk@gmail.com>
2014-12-16 09:33:45 +1300

    Adding 3010 as valid exit code
    3010 indicated a reboot is required, adding as valid exit code so that
    chocolatey does not spew errors on install.

--- vmware-horizon-client/tools/chocolateyInstall.ps1
+++ vmware-horizon-client/tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $installerType = 'EXE'
 $url = 'https://download3.vmware.com/software/view/viewclients/CART14Q4/VMware-Horizon-View-Client-x86-3.2.0-2331566.exe'
 $url64 = 'https://download3.vmware.com/software/view/viewclients/CART14Q4/VMware-Horizon-View-Client-x86_64-3.2.0-2331566.exe'
 $silentArgs = '/S /V"/qn REBOOT=Reallysuppress"'
-$validExitCodes = @(0)
+$validExitCodes = @(0, 3010)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
