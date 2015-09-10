f84b6b59cafe477ac824f199ab82076da9355cff
Joshua King <windosbk+github@gmail.com>
2015-09-11 11:42:44 +1200

    Horizon Client 3.5

--- vmware-horizon-client/tools/chocolateyInstall.ps1
+++ vmware-horizon-client/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'vmware-horizon-client'
 $installerType = 'EXE'
-$url = 'https://download3.vmware.com/software/view/viewclients/CART15Q2/VMware-Horizon-View-Client-x86-3.4.0-2769709.exe'
-$url64 = 'https://download3.vmware.com/software/view/viewclients/CART15Q2/VMware-Horizon-View-Client-x86_64-3.4.0-2769709.exe'
+$url = 'https://download3.vmware.com/software/view/viewclients/CART15Q3/VMware-Horizon-View-Client-x86-3.5.0-2999900.exe'
+$url64 = 'https://download3.vmware.com/software/view/viewclients/CART15Q3/VMware-Horizon-View-Client-x86_64-3.5.0-2999900.exe'
 $silentArgs = '/S /V"/qn REBOOT=Reallysuppress"'
 $validExitCodes = @(0, 3010)
 
