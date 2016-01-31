ecc6bdc1d4e1ae9ac3a4247785b4f5d313b47f11
Joshua King <windosbk+github@gmail.com>
2016-02-01 12:08:28 +1300

    closespy 3.23

--- clonespy/tools/chocolateyInstall.ps1
+++ clonespy/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'clonespy'
 $installerType = 'EXE'
-$url = 'http://www.clonespy.com/english/download/cspy322.exe'
+$url = 'http://www.clonespy.com/english/download/cspy323.exe'
 $silentArgs = '/S /NODESKTOPSHORTCUT /X64'
 $validExitCodes = @(0)
 
