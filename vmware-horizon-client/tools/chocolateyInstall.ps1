3b57d0edacfb860535ee0ab4d3091a0cf0341abf
Joshua King <windosbk+github@gmail.com>
2016-04-04 08:21:42 +1200

    and 4.0.1 just a couple days later

--- vmware-horizon-client/tools/chocolateyInstall.ps1
+++ vmware-horizon-client/tools/chocolateyInstall.ps1
@@ -6,16 +6,16 @@ $packageArgs = @{
   packageName   = 'vmware-horizon-client'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://download3.vmware.com/software/view/viewclients/CART16Q1/VMware-Horizon-Client-x86-4.0.0-3677815.exe'
-  url64bit      = 'https://download3.vmware.com/software/view/viewclients/CART16Q1/VMware-Horizon-Client-x86_64-4.0.0-3677815.exe'
+  url           = 'https://download3.vmware.com/software/view/viewclients/CART16Q1_2/VMware-Horizon-Client-x86-4.0.1-3698521.exe'
+  url64bit      = 'https://download3.vmware.com/software/view/viewclients/CART16Q1_2/VMware-Horizon-Client-x86_64-4.0.1-3698521.exe'
 
   silentArgs    = '/S /V"/qn REBOOT=Reallysuppress"'
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'vmware-horizon-client*'
-  checksum      = '630d7c339a4c76918d3f8b736c3cd0cf'
+  checksum      = 'b8924ae95437865203243f06fd05f82e'
   checksumType  = 'md5'
-  checksum64    = 'ba945431043999a3811b62f313973d2d'
+  checksum64    = '58e1b8ff513aafe2a4ce2d8955d76ace'
   checksumType64= 'md5'
 }
 
