12d96698d30a731c70fec5b3b66b655f42fb4006
Joshua King <windosbk+github@gmail.com>
2016-05-25 12:03:56 +1200

    clonespy 3.24

--- clonespy/tools/chocolateyInstall.ps1
+++ clonespy/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'clonespy'
 $installerType = 'EXE'
-$url = 'http://www.clonespy.com/english/download/cspy323.exe'
+$url = 'http://www.clonespy.com/english/download/cspy324.exe'
 $silentArgs = '/S /NODESKTOPSHORTCUT /X64'
 $validExitCodes = @(0)
 
