35605519c6eba065a5850845c5f8df423303eca8
Joshua King <windosbk+github@gmail.com>
2016-09-22 10:28:07 +1200

    New Horizon Client

--- vmware-horizon-client/tools/chocolateyInstall.ps1
+++ vmware-horizon-client/tools/chocolateyInstall.ps1
@@ -6,16 +6,16 @@ $packageArgs = @{
   packageName   = 'vmware-horizon-client'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://download3.vmware.com/software/view/viewclients/CART16Q2/VMware-Horizon-Client-x86-4.1.0-3977225.exe'
-  url64bit      = 'https://download3.vmware.com/software/view/viewclients/CART16Q2/VMware-Horizon-Client-x86_64-4.1.0-3977225.exe'
+  url           = 'https://download3.vmware.com/software/view/viewclients/CART16Q3/VMware-Horizon-Client-x86-4.2.0-4336726.exe'
+  url64bit      = 'https://download3.vmware.com/software/view/viewclients/CART16Q3/VMware-Horizon-Client-x86_64-4.2.0-4336726.exe'
 
   silentArgs    = '/S /V"/qn REBOOT=Reallysuppress"'
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'vmware-horizon-client*'
-  checksum      = '28839eadfaba594f5cf0cf62f4d8c830'
+  checksum      = '9b61cb97e9877771c540ac6cc43831fa'
   checksumType  = 'md5'
-  checksum64    = '3ca893d2bce82e0703b9d50c863350b1'
+  checksum64    = 'd190274ed5a651671bbb3ae8aa7a88f4'
   checksumType64= 'md5'
 }
 
