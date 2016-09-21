f0c422cedfc1c09d20f7c2685c23acbd836d378d
Joshua King <windosbk+github@gmail.com>
2016-09-22 10:50:59 +1200

    new clonespy version

--- clonespy/tools/chocolateyInstall.ps1
+++ clonespy/tools/chocolateyInstall.ps1
@@ -1,7 +1,18 @@
-﻿$packageName = 'clonespy'
-$installerType = 'EXE'
-$url = 'http://www.clonespy.com/english/download/cspy324.exe'
-$silentArgs = '/S /NODESKTOPSHORTCUT /X64'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = 'clonespy'
+  fileType      = 'exe'
+  url           = 'http://www.clonespy.com/english/download/cspy33.exe'
+
+  silentArgs    = '/S /NODESKTOPSHORTCUT /X64'
+  validExitCodes= @(0)
+
+  softwareName  = 'clonespy*'
+  checksum      = '71A479CF3DDD91C5E00B8B834A87062B'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
