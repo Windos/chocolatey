5bb1803875aa830aedb47faa8921a87c1e52f234
Joshua King <windosbk+github@gmail.com>
2017-01-30 21:37:31 +1300

    fixes for cinebench

--- cinebench/tools/chocolateyInstall.ps1
+++ cinebench/tools/chocolateyInstall.ps1
@@ -1,20 +1,8 @@
 ﻿$packageName = 'cinebench'
-$url = 'http://http.maxon.net/pub/benchmarks/CINEBENCH_R15.zip'
-$md5Hash = '0a7353398a7f4b61f3b970c74feef726'
+$url = 'https://http.maxon.net/pub/benchmarks/CINEBENCHR15.038.zip'
+$md5Hash = 'E254FE22253DFD81EAECCD566CAFDE60'
 
-try { 
-  $binRoot = "$env:systemdrive\tools"
-  if($env:chocolatey_bin_root -ne $null -and $env:chocolatey_bin_root -notlike '*:\*'){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $installDir = Join-Path $binRoot "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
+$installDir = $env:chocolateyPackageFolder
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -checksum $md5Hash
-  Install-ChocolateyDesktopLink -TargetFilePath "$installDir\CINEBENCH Windows 64 Bit.exe"
-  
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
-}
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$installDir" -checksum $md5Hash
+Install-ChocolateyDesktopLink -TargetFilePath "$installDir\CINEBENCH R15.038_RC184115\CINEBENCH Windows 64 Bit.exe"
