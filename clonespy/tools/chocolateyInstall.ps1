c798415678515207f5b27a319e6cf517a9ac211c
Joshua King <windosbk+github@gmail.com>
2017-01-30 20:29:41 +1300

    Clonespy 3.33

--- clonespy/tools/chocolateyInstall.ps1
+++ clonespy/tools/chocolateyInstall.ps1
@@ -5,13 +5,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = 'clonespy'
   fileType      = 'exe'
-  url           = 'http://www.clonespy.com/english/download/cspy331.exe'
+  url           = 'http://www.clonespy.com/english/download/cspy333.exe'
 
   silentArgs    = '/S /NODESKTOPSHORTCUT /X64'
   validExitCodes= @(0)
 
   softwareName  = 'clonespy*'
-  checksum      = '9BF18E073963334270C4E6E567DC70BC'
+  checksum      = 'BC452F8C0FE01C289C98BB4633B87C4E'
   checksumType  = 'md5'
 }
 
