d306d0abde9e2470cd15cb51963f0d6b1d5e8736
Windos <windosbk+github@gmail.com>
2018-01-09 07:23:02 +1300

    horizon 4.7

--- vmware-horizon-client/tools/chocolateyInstall.ps1
+++ vmware-horizon-client/tools/chocolateyInstall.ps1
@@ -6,13 +6,13 @@ $packageArgs = @{
   packageName   = 'vmware-horizon-client'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://download3.vmware.com/software/view/viewclients/CART18FQ3/VMware-Horizon-Client-4.6.1-6748947.exe'
+  url           = 'https://download3.vmware.com/software/view/viewclients/CART18FQ4/VMware-Horizon-Client-4.7.0-7395453.exe'
 
   silentArgs    = '/silent /norestart'
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'vmware-horizon-client*'
-  checksum      = '34291119335b905ef8c70414b292b60e'
+  checksum      = '19c97592b7a9eda43d9bf43355f95c53'
   checksumType  = 'md5'
 }
 
