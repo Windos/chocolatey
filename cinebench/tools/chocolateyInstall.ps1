7dbab2e8449d91e6fe69a9c84f6a1502de1fb99f
Josh King <joshuak@hdc.govt.nz>
2019-06-18 19:12:30 +1200

    cinebench r20

--- cinebench/tools/chocolateyInstall.ps1
+++ cinebench/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'cinebench'
-$url = 'https://http.maxon.net/pub/benchmarks/CINEBENCHR15.038.zip'
-$md5Hash = 'E254FE22253DFD81EAECCD566CAFDE60'
+$url = 'http://http.maxon.net/pub/cinebench/CinebenchR20.zip'
+$md5Hash = 'F6FA0F554CEBC22F10D91F1D148710DE'
 
 $installDir = $env:chocolateyPackageFolder
 
 Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$installDir" -checksum $md5Hash
-Install-ChocolateyDesktopLink -TargetFilePath "$installDir\CINEBENCH R15.038_RC184115\CINEBENCH Windows 64 Bit.exe"
+Install-ChocolateyDesktopLink -TargetFilePath "$installDir\CinebenchR20\Cinebench.exe"
