e6c8c64d7935d2b6445d5cc5139b05061978a98e
Windos <Windos@users.noreply.github.com>
2021-08-07 00:13:00 +0000

    Apply Choco package update - onedarkpro-vscode 3.11.3

--- _auto/onedarkpro-vscode/tools/chocolateyinstall.ps1
+++ _auto/onedarkpro-vscode/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -ExtensionId 'zhuangtongfa.Material-theme@3.11.2'
+Install-VsCodeExtension -ExtensionId 'zhuangtongfa.Material-theme@3.11.3'
