47d0526fe2158c4abb0735fa0fc846630ecf32d2
Windos <Windos@users.noreply.github.com>
2024-11-14 01:03:29 +0000

    Apply Choco package update - azurefunctions-vscode 1.16.1

--- _auto/azurefunctions-vscode/tools/chocolateyinstall.ps1
+++ _auto/azurefunctions-vscode/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -ExtensionId 'ms-azuretools.vscode-azurefunctions@1.16.0'
+Install-VsCodeExtension -ExtensionId 'ms-azuretools.vscode-azurefunctions@1.16.1'
